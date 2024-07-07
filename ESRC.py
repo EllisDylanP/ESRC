759eb6ef4c0bd4d6093f2d0b1001dd7cce34b824
Dylan Peter Ellis <dylanpeterellis@gmail.com>
2024-07-07 00:11:04 -0400

    Initial Deployment

---
+++ ESRC.py
@@ -0,0 +1,62 @@
+import pandas as pandas
+import streamlit as streamlit
+import plotly.express as px
+from PIL import Image
+
+## PAGE OUTLINE
+streamlit.set_page_config(page_title='The Endocannabinoid System Research Company')
+streamlit.header("The Endocannabinoid System Research Company - User Portal")
+streamlit.subheader("Biometric Analysis, Industry Outreach, and Publications are available")
+
+## DATA
+excel_file = "Retail face of the Endocannabinoid System Research Company.xlsm"
+sheetforlogin = "Sheet3"
+sheetforjournal = "Sheet8"
+sheetforreporting = "Sheet13"
+df_loginpage = pandas.read_excel(excel_file, engine = "openpyxl", usecols= "A, B", sheet_name=sheetforlogin, header = 4)
+df_journal = pandas.read_excel(excel_file, sheet_name = sheetforjournal, header= 5)
+df = pandas.read_excel(excel_file, sheet_name=sheetforreporting, usecols='A:H', header = 4)
+df_financial = pandas.read_excel(excel_file, sheet_name=sheetforreporting, usecols="A, B, C, E", header = 4)
+df_HR = pandas.read_excel(excel_file, sheet_name=sheetforreporting, usecols='A, B, C, F', header = 4)
+df_Oxy = pandas.read_excel(excel_file, sheet_name=sheetforreporting, usecols='A, B, C, G', header = 4)
+df_PI = pandas.read_excel(excel_file, sheet_name=sheetforreporting, usecols='A, B, C, H', header = 4)
+
+## LOGIN USER INTERFACE
+def loginprocess (username, password):
+    userexists = ((df_loginpage["USER ID"] == username) & (df_loginpage["PASSWORD"] == password)).any()
+    if not userexists:
+        return "Authentication failed"
+    
+    # Get indices or rows from the filtered reference DataFrame
+    filtered_indices = df[df.iloc[:, 0] == username].index
+        
+    # Filter other data tables using the filtered indices or rows
+    HRreport = px.line(df_HR.loc[filtered_indices],  x = "Date", y = "Heart rate", title = "Heart Rate Metrics")
+    Oxyreport = px.line(df_Oxy.loc[filtered_indices],   x = "Date", y = "Oxygen Saturation", title = "Oxygen Saturation Metrics")
+    PIreport = px.line(df_PI.loc[filtered_indices],  x = "Date", y = "Perfusion Index", title = "Perfusion Index report")
+    financialreport = px.line(df_financial.loc[filtered_indices], x="Date", y="Price", title = "Financial Report")
+        
+    # Prepare data for line graphs in Streamlit (assuming simple structure for demonstration)
+    streamlit_graphs = {
+            streamlit.plotly_chart(HRreport),
+            streamlit.plotly_chart(Oxyreport),
+            streamlit.plotly_chart(PIreport),
+            streamlit.plotly_chart(financialreport)
+        }
+    return filtered_indices
+
+page = streamlit.sidebar.selectbox("Choose a page", ['Analysis', 'Journal'])
+if page == "Analysis":
+    with streamlit.form("Login"):
+        username = streamlit.text_input(label ="Username")
+        password = streamlit.text_input(label = "Password", type = "password")
+        submit_button = streamlit.form_submit_button(label="Login")
+        if submit_button:
+            loginprocess(username, password)
+            #streamlit.df_financial.multiselect("Select the product:",options = df["Product"].unique(),default = df["Product"].unique())
+elif page == "Journal":
+    streamlit.dataframe(df_journal)
+
+
+DPEimage = Image.open("assets/DylanPeterEllislogo.jfif")
+streamlit.image(DPEimage, caption = "Dylan Peter Ellis", use_column_width= 100)
