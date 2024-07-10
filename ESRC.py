7b4d9acce05e54db12862277e5f89322a3bad13b
Dylan Ellis <112646921+EllisDylanP@users.noreply.github.com>
2024-07-10 10:51:49 -0400

    ESRC.py
    this removes the "text-align:center" of the header line.
    
    07/10/2024 10:51AM

--- ESRC.py
+++ ESRC.py
@@ -15,7 +15,7 @@ streamlit.markdown(
     unsafe_allow_html=True)
 streamlit.markdown('<h1 class="title">The Endocannabinoid System Research Company</h1>', unsafe_allow_html=True)
 #streamlit.set_page_config(page_title='The Endocannabinoid System Research Company')
-streamlit.header("The Endocannabinoid System Research Company - User Portal", text-align:center)
+streamlit.header("The Endocannabinoid System Research Company - User Portal")
 streamlit.subheader("Biometric Analysis, Industry Outreach, and Publications are available")
 
 ## DATA
