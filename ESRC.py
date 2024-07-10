013a8cc10a111c4191118b065cd3ff2e0f07bdd2
Dylan Ellis <112646921+EllisDylanP@users.noreply.github.com>
2024-07-10 14:31:53 -0400

    ESRC.py
    This changed df_foranyfunction to df_forallfunction and added ")", before .all(), in the equivalent of the dataframe.
    
    07/10/2024 14:31PM

--- ESRC.py
+++ ESRC.py
@@ -40,7 +40,7 @@ def loginprocess (username, password):
     filtered_index = df[df.iloc[:, 0] == username].index
     new_df = df[df.iloc[:, 0] == username]
     df_product = streamlit.multiselect("Select the product:",options = new_df["Product"].unique(),default = new_df["Product"].unique())
-    df_foranyfunction = ((df["USER ID"] == new_df) & (df["Product"] == new_df[df_product]).all()
+    df_forallfunction = ((df["USER ID"] == new_df) & (df["Product"] == new_df[df_product])).all()
     filtered_indices = df_foranyfunction.index
     
         
