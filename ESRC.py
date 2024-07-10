961f75d23afeeae109a42fad0dd57c6b6ecbfbc4
Dylan Ellis <112646921+EllisDylanP@users.noreply.github.com>
2024-07-10 19:15:37 -0400

    ESRC.py
    This update removes the "left, right" expression on line 45, previously, and adds the dataframes:
    
    streamlit.dataframe(new_df)
    streamlit.dataframe(df_product)
    streamlit.dataframe(df_forallfunction)
    
    07/10/2024 19:15PM

--- ESRC.py
+++ ESRC.py
@@ -42,7 +42,9 @@ def loginprocess (username, password):
     df_product = streamlit.multiselect("Select the product:",options = new_df["Product"].unique(),default = new_df["Product"].unique())
     df_forallfunction = ((df["USER ID"] == new_df) & (df["Product"] == new_df[df_product])).all()
     filtered_indices = df_foranyfunction.index
-    left, right = left.align(right, axis=1, copy=False)
+    streamlit.dataframe(new_df)
+    streamlit.dataframe(df_product)
+    streamlit.dataframe(df_forallfunction)
     
         
     # Filter other data tables using the filtered indices or rows
