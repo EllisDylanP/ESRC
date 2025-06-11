fc6265ac99aee44ee4dedc7feaa900df4fa3b8b6
Dylan Ellis <112646921+EllisDylanP@users.noreply.github.com>
2025-06-11 18:29:51 -0400

    ESRC.py
    no streamlit.fileuploader()
    
    06/11/2025 18:29PM

--- ESRC.py
+++ ESRC.py
@@ -18,18 +18,18 @@ streamlit.markdown('<h1 class="title">The Endocannabinoid System Research Compan
 streamlit.header("User Portal - Access to Biometric Analysis, Industry Outreach, and Publications")
 
 ## DATA
-UsernameAndPassword = streamlit.file_uploader("Username and Password.csv")
-for UsernameAndPassword in UsernameAndPassword:
+UsernameAndPassword = "Username and Password.csv"
+##for UsernameAndPassword in UsernameAndPassword:
     df_loginpage = pandas.read_csv(UsernameAndPassword, usecols= "A, B", header = 4)
-Activity = streamlit.file_uploader("Activity.csv")
-for Activity in Activity:
+Activity = "Activity.csv"
+##for Activity in Activity:
     df = pandas.read_csv(Activity, usecols='A:H', header = 4)
     df_financial = pandas.read_csv(Activity, usecols="A, B, C, E", header = 4)
     df_HR = pandas.read_csv(Activity, usecols='A, B, C, F', header = 4)
     df_Oxy = pandas.read_csv(Activity, usecols='A, B, C, G', header = 4)
     df_PI = pandas.read_csv(Activity, usecols='A, B, C, H', header = 4)
-Journal = streamlit.file_uploader("Journal.csv")
-for Journal in Journal:
+Journal = "Journal.csv"
+##for Journal in Journal:
     df_journal = pandas.read_csv(Journal, header= 7)
 
 with streamlit.form(key = "Registration"):
