be9f8d12bacd47346d0014aaf7e60c6ac5f6ba7c
Dylan Ellis <112646921+EllisDylanP@users.noreply.github.com>
2025-06-11 18:31:01 -0400

    ESRC.py
    No indent with df_loginpage
    
    06/11/2025 18:30PM

--- ESRC.py
+++ ESRC.py
@@ -20,17 +20,17 @@ streamlit.header("User Portal - Access to Biometric Analysis, Industry Outreach,
 ## DATA
 UsernameAndPassword = "Username and Password.csv"
 ##for UsernameAndPassword in UsernameAndPassword:
-    df_loginpage = pandas.read_csv(UsernameAndPassword, usecols= "A, B", header = 4)
+df_loginpage = pandas.read_csv(UsernameAndPassword, usecols= "A, B", header = 4)
 Activity = "Activity.csv"
 ##for Activity in Activity:
-    df = pandas.read_csv(Activity, usecols='A:H', header = 4)
-    df_financial = pandas.read_csv(Activity, usecols="A, B, C, E", header = 4)
-    df_HR = pandas.read_csv(Activity, usecols='A, B, C, F', header = 4)
-    df_Oxy = pandas.read_csv(Activity, usecols='A, B, C, G', header = 4)
-    df_PI = pandas.read_csv(Activity, usecols='A, B, C, H', header = 4)
+df = pandas.read_csv(Activity, usecols='A:H', header = 4)
+df_financial = pandas.read_csv(Activity, usecols="A, B, C, E", header = 4)
+df_HR = pandas.read_csv(Activity, usecols='A, B, C, F', header = 4)
+df_Oxy = pandas.read_csv(Activity, usecols='A, B, C, G', header = 4)
+df_PI = pandas.read_csv(Activity, usecols='A, B, C, H', header = 4)
 Journal = "Journal.csv"
 ##for Journal in Journal:
-    df_journal = pandas.read_csv(Journal, header= 7)
+df_journal = pandas.read_csv(Journal, header= 7)
 
 with streamlit.form(key = "Registration"):
     Username=streamlit.text_input("Username")
