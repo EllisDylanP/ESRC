f480a7f15f5008478314262a6a70dfd7b24f80fc
Dylan Ellis <112646921+EllisDylanP@users.noreply.github.com>
2026-04-22 02:21:03 -0400

    ESRC.py
    cleaned

--- ESRC.py
+++ ESRC.py
@@ -1,33 +1,19 @@
 import streamlit as streamlit
-from PIL import Image
-import streamlit.components.v1
 
-## FRONT PAGE DESIGN
-streamlit.markdown(
-    """
-    <style>
-    .title {
-        text-align: center;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True)
-streamlit.markdown('<h1 class="title"><b>Dylan Peter Ellis</b></h1>', unsafe_allow_html=True)
-streamlit.markdown('<h3 class="title"><b>The Business of Biochemistry</b></h3>', unsafe_allow_html=True)
-streamlit.markdown('<h4 class="title"><u>The Endocannabinoid System Research Company - Chemical Sensing Initiative</u></h4>', unsafe_allow_html=True)
-streamlit.markdown('<h4 class="title"><u>The Holy Grace</u></h4>', unsafe_allow_html=True)
+streamlit.header(The Endocannabinoid System Research Company - Chemical Sensing Initiative)
+streamlit.subheader(The Holy Grace)
 
-streamlit.write(-The heart)
-streamlit.write(-Nitric Oxide)
-streamlit.write(-Endocannabinoids)
+streamlit.text(-The heart)
+streamlit.text(-Nitric Oxide)
+streamlit.text(-Endocannabinoids)
 
-streamlit.write(Circulation >>> dynamics >>> tissue engineering )
+streamlit.text(Circulation >>> dynamics >>> tissue engineering )
 
-streamlit.write(Nitric oxide presence by the matter of light at the device’s receptor. Gas chromatography-mass 
+streamlit.text(Nitric oxide presence by the matter of light at the device’s receptor. Gas chromatography-mass 
 spectrometry (GC-MS) by the work of a film. Presence of mind at the film. Contrast of the film to 
 behavior.)
 
-streamlit.write(Endocannabinoids are light dependent variables. Nitric oxide is present as a plume. Concentration is 
+streamlit.text(Endocannabinoids are light dependent variables. Nitric oxide is present as a plume. Concentration is 
 displayed as wavelength.)
 
-streamlit.write(The watch is made integrated.)
+streamlit.text(The watch is made integrated.)
