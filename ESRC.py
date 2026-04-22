f1e94018d5fc46481f017aedd2bbe776e78a89a2
Dylan Ellis <112646921+EllisDylanP@users.noreply.github.com>
2026-04-22 02:25:49 -0400

    ESRC.py
    ""

--- ESRC.py
+++ ESRC.py
@@ -3,17 +3,17 @@ import streamlit as streamlit
 streamlit.header("The Endocannabinoid System Research Company - Chemical Sensing Initiative")
 streamlit.subheader("The Holy Grace")
 
-streamlit.text(-The heart)
-streamlit.text(-Nitric Oxide)
-streamlit.text(-Endocannabinoids)
+streamlit.text("-The heart")
+streamlit.text("-Nitric Oxide")
+streamlit.text("-Endocannabinoids")
 
-streamlit.text(Circulation >>> dynamics >>> tissue engineering )
+streamlit.text("Circulation >>> dynamics >>> tissue engineering")
 
-streamlit.text(Nitric oxide presence by the matter of light at the device\’s receptor. Gas chromatography-mass 
+streamlit.text("Nitric oxide presence by the matter of light at the device\’s receptor. Gas chromatography-mass 
 spectrometry (GC-MS) by the work of a film. Presence of mind at the film. Contrast of the film to 
-behavior.)
+behavior.")
 
-streamlit.text(Endocannabinoids are light dependent variables. Nitric oxide is present as a plume. Concentration is 
-displayed as wavelength.)
+streamlit.text("Endocannabinoids are light dependent variables. Nitric oxide is present as a plume. Concentration is 
+displayed as wavelength.")
 
-streamlit.text(The watch is made integrated.)
+streamlit.text("The watch is made integrated.")
