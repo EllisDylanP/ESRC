e89f7230872bf603eebf9e82994bccc9a4406e3a
Dylan Ellis <112646921+EllisDylanP@users.noreply.github.com>
2026-05-17 16:15:22 -0400

    ESRC.py
    text

--- ESRC.py
+++ ESRC.py
@@ -14,4 +14,4 @@ streamlit.text("Seek an endocannabinoid.")
 streamlit.image("The Watch.gif")
 
 
-streamlit.subheader("Contact Dylan by phone at 7039014281 or by email at dpe.esrc@gmail.com for questions regarding work with the ESRC.")
+streamlit.text("Contact Dylan by phone at 7039014281 or by email at dpe.esrc@gmail.com for questions regarding work with the ESRC.")
