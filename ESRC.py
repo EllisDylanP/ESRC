b1e55046a793e6132fd49cedefe43f7db347ac5d
Dylan Ellis <112646921+EllisDylanP@users.noreply.github.com>
2026-05-17 13:51:42 -0400

    ESRC.py
    UV Vis Spectrophotometry

--- ESRC.py
+++ ESRC.py
@@ -9,7 +9,7 @@ streamlit.text("-Nitric Oxide")
 
 streamlit.text("Circulation >>> dynamics >>> tissue engineering")
 
-streamlit.text("Nitric oxide presence by the matter of light at the device’s receptor. Gas chromatography-mass spectrometry (GC-MS) by the work of a film. Presence of mind at the film. Contrast of the film to behavior.")
+streamlit.text("Nitric oxide presence by the matter of light at the device’s receptor. UV-Vis Spectrophotometry by the work of a film. Presence of mind at the film. Contrast of the film to behavior.")
 
 streamlit.text("Endocannabinoids are light dependent variables. Nitric oxide is present as a plume. Concentration is displayed as wavelength.")
 
