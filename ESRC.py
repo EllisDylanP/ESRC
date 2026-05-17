b6b3339959dc983d9ccc685fe13a748328efdf74
Dylan Ellis <112646921+EllisDylanP@users.noreply.github.com>
2026-05-17 16:29:42 -0400

    ESRC.py
    Excellence

--- ESRC.py
+++ ESRC.py
@@ -1,9 +1,9 @@
 import streamlit as streamlit
 streamlit.header("The Holy Grace")
-streamlit.text("-Endocannabinoid")
 streamlit.text("-Haemoglobin")
 streamlit.text("-Nitric Oxide")
-streamlit.text("Circulation >>> dynamics >>> tissue engineering")
-streamlit.text("Nitric oxide presence by the matter of light at the device’s receptor. UV-Vis Spectrophotometry by the work of a film. Presence of mind at the film. Contrast of the film to behavior.")
+streamlit.text("-Endocannabinoid")
+streamlit.text("Blood >>> Muscle >>> Fascia")
+streamlit.text("Work to watch. Movement moving movement. Personification of might.")
 streamlit.image("The Watch.gif")
-streamlit.text("You may contact the ESRC at 7039014281 and dpe.esrc@gmail.com.")
+streamlit.text("You may email the ESRC at dpe.esrc@gmail.com.")
